1ca6c56758fc210059af1e21bc82bbe4079324b4
Blake Bridges <71102189+BlakeBridges@users.noreply.github.com>
2022-04-23 11:32:59 -0700

    Deleted extra file that was un needed
    was from a test

--- hello_flask/compare.py
+++
@@ -1,8 +0,0 @@
-import bcrypt
-pw = "testing4"
-salted = bcrypt.hashpw(bytes(pw, 'utf-8'), bcrypt.gensalt(12))
-print(salted)
-print(bcrypt.checkpw(bytes(pw, 'utf-8'), salted))
-print(b'$2b$12$MdrtqZn1KrFoCIN3QLnFcOmc4ZgoAsUJ3HyfzgniLlEPRXC2L1Qvq' == salted)
-
-
